988b1fdd44d1ee1debbc8f2136d5a8c7cd34d658
mrin2810 <mrinmayeerane2810@gmail.com>
2021-02-27 19:09:16 +0530

    optimized code

--- script.js
+++ script.js
@@ -6,38 +6,23 @@ const image2 = document.getElementById('image2');
 const image3 = document.getElementById('image3');
 const textBox = document.getElementById('text-box');
 
-// Dark Mode function Styles
-function darkMode() {
-    nav.style.backgroundColor = 'rgb(0 0 0 /70%)';
-    textBox.style.backgroundColor = 'rgb(255 255 255 /70%)';
-    toggleIcon.children[0].textContent = "Dark Mode";
-    toggleIcon.children[1].classList.remove('fa-sun');
-    toggleIcon.children[1].classList.add('fa-moon');
-    image1.src = "img/undraw_proud_coder_dark.svg";
-    image2.src = "img/undraw_feeling_proud_dark.svg";
-    image3.src = "img/undraw_conceptual_idea_dark.svg";
-}
-// Light Mode function Styles
-function lightMode() {
-    textBox.style.backgroundColor = 'rgb(0 0 0 /70%)';
-    nav.style.backgroundColor = 'rgb(255 255 255 /70%)';
-    toggleIcon.children[0].textContent = "Light Mode";
-    toggleIcon.children[1].classList.remove('fa-moon');
-    toggleIcon.children[1].classList.add('fa-sun');
-    image1.src = "img/undraw_proud_coder_light.svg";
-    image2.src = "img/undraw_feeling_proud_light.svg";
-    image3.src = "img/undraw_conceptual_idea_light.svg";
-}
-
-// Switch theme dynamically
 function switchTheme(event) {
-    if(event.target.checked){
-        document.documentElement.setAttribute('data-theme', 'dark');
-        darkMode();
-    }else{
-        document.documentElement.setAttribute('data-theme', 'light');
-        lightMode();
+    let theme = 'light';
+    if(!event.target.checked){
+        textBox.style.backgroundColor = 'rgb(0 0 0 /70%)';
+        nav.style.backgroundColor = 'rgb(255 255 255 /70%)';
+        toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    } else {
+        theme = 'dark';
+        nav.style.backgroundColor = 'rgb(0 0 0 /70%)';
+        textBox.style.backgroundColor = 'rgb(255 255 255 /70%)';
+        toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
     }
+    toggleIcon.children[0].textContent = `${theme} Mode`;
+    image1.src = `img/undraw_proud_coder_${theme}.svg`;
+    image2.src = `img/undraw_feeling_proud_${theme}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${theme}.svg`;
+    document.documentElement.setAttribute('data-theme', theme);
 }
 
 // Event Listener
